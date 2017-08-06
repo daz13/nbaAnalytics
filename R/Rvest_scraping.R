92ee525a68e5682a7988f4536403bcd7498bdfd2
Unknown <david.zilian@googlemail.com>
2017-08-06 14:21:46 +0200

    Rvest dev continued

--- R/Rvest_scraping.R
+++ R/Rvest_scraping.R
@@ -2,6 +2,8 @@
 #' # Test Rvest craping from BB refc.om
 #'
 #'
+library(dplyr)
+library(rvest)
 
 read_table <-  function(url, html_container){
   read_html(url) %>%
@@ -19,19 +21,35 @@ read_table("https://www.basketball-reference.com/teams/ATL/", html_container = "
 #' # Players
 #'
 #+
-read_table("https://www.basketball-reference.com/players/a/", "#div_players") -> players_a
+
+mapply(letters[-24], FUN = function(x) {print(x)
+  read_table(paste0("https://www.basketball-reference.com/players/",x,"/"), "#div_players") }) -> all_players
+all_players %>%  bind_rows() -> all_players
+
 
 gen_id <- function(name) {
-  name = "Dirk Nowitzki"
   stringr::str_split(string = name, pattern = " ") %>% unlist()-> name_split
 
   (name_split[length(name_split)] %>% strsplit("") %>% unlist())[1:5] %>% paste(collapse = "") %>% tolower() -> five
   (name_split[1] %>% strsplit("") %>% unlist())[1:2] %>% paste(collapse = "") %>% tolower() -> two
 
-  paste0(five,two)
+  paste0(five,two,"01")
 }
 
-players_a %>% mutate(id_521 = )
+all_players %>% mutate(html_id = as.character(purrr::map(Player, gen_id)) )-> all_players
+
+all_players %>% group_by(html_id) %>% filter(n() > 3)
+
+rollapply <- function(x, n, f, ...) {
+  out <- rep(NA, length(x))
+
+  offset <- trunc(n / 2)
+  for (i in (offset + 1):(length(x) - n + offset + 1)) {
+    out[i] <- f(x[(i - offset):(i + offset - 1)], ...)
+  }
+  out
+}
+
 
 #' #Games
 #'
